1842175ab90acb5848a098c223f806efac0a3632
Thomas Ersfeld <thomas.ersfeld@gmail.com>
2014-04-27 20:23:51 +0200

    indent

--- rmi/MSTMainFrame.java
+++ rmi/MSTMainFrame.java
@@ -9,57 +9,57 @@ public class MSTMainFrame extends JFrame
     private JTextPane textPane;
     private JTextField textField;
     
-  public MSTMainFrame()
-  {
-    this.setTitle("MST");
-    this.setSize(640, 400);
-    this.setLocationRelativeTo(null);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
-    this.setVisible(true);
-    
-    //Instanciation d'un objet JPanel
-    JPanel pan = new JPanel();
-    pan.setBorder(new EmptyBorder(10, 10, 10, 10) );
-    pan.setLayout(new BoxLayout(pan, BoxLayout.Y_AXIS));
-    
-    textPane = new JTextPane();
-    textPane.setBorder(new EmptyBorder(10, 10, 10, 10) );
-    pan.add(new JScrollPane(textPane));
-    
-    DefaultCaret caret = (DefaultCaret)textPane.getCaret();
-    caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-    
-    textField = new JTextField();
-    textField.setMaximumSize(
-        new Dimension(Integer.MAX_VALUE,
-        textField.getPreferredSize().height));
-    pan.add(textField);
-       
-    //On prévient notre JFrame que notre JPanel sera son content pane
-    this.setContentPane(pan); 
-    
-    InitStyles(); 
-  }
+    public MSTMainFrame()
+    {
+        this.setTitle("MST");
+        this.setSize(640, 400);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+        this.setVisible(true);
+        
+        //Instanciation d'un objet JPanel
+        JPanel pan = new JPanel();
+        pan.setBorder(new EmptyBorder(10, 10, 10, 10) );s
+        pan.setLayout(new BoxLayout(pan, BoxLayout.Y_AXIS));
+        
+        textPane = new JTextPane();
+        textPane.setBorder(new EmptyBorder(10, 10, 10, 10) );
+        pan.add(new JScrollPane(textPane));
+        
+        DefaultCaret caret = (DefaultCaret)textPane.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        
+        textField = new JTextField();
+        textField.setMaximumSize(
+            new Dimension(Integer.MAX_VALUE,
+            textField.getPreferredSize().height));
+        pan.add(textField);
+           
+        //On prévient notre JFrame que notre JPanel sera son content pane
+        this.setContentPane(pan); 
+        
+        InitStyles(); 
+    }
   
-  public JTextField GetTextField()
-  {
-      return textField;
-  }
-  
-  public void InitStyles()
-  {
+    public JTextField GetTextField()
+    {
+        return textField;
+    }
+
+    public void InitStyles()
+    {
         Style style = textPane.addStyle("info", textPane.getStyle("default"));
         StyleConstants.setForeground(style, Color.BLUE);
-        
+
         style = textPane.addStyle("error", textPane.getStyle("default"));
         StyleConstants.setForeground(style, Color.RED);
-        
+
         style = textPane.addStyle("sent_message", textPane.getStyle("default"));
         StyleConstants.setForeground(style, Color.GREEN);
-        
+
         style = textPane.addStyle("received_message", textPane.getStyle("default"));
         StyleConstants.setForeground(style, Color.BLACK);
-  }
+    }
     
     public void Print(String msg, String style)
     {
